6649dee0339868958e0867af23cc30b32a9c6665
NJP-10 <nicholasjeneperan@outlook.com>
2026-05-15 19:02:08 +0800

    wip: changed gallery png to webp

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,55 +1,55 @@
 const gallery = [
     {
       '_id': '1',
-      'image': '/images/1.png',
+      'image': '/images/1.webp',
     },
     {
       '_id': '2',
-      'image': '/images/2.png',  
+      'image': '/images/2.webp',  
     },
     {
       '_id': '3',
-      'image': '/images/3.png',
+      'image': '/images/3.webp',
     },
     {
       '_id': '4',
-      'image': '/images/4.png',
+      'image': '/images/4.webp',
     },
     {
       '_id': '5',
-      'image': '/images/5.png',  
+      'image': '/images/5.webp',  
     },
     {
       '_id': '6',
-      'image': '/images/6.png',    
+      'image': '/images/6.webp',    
     },
     {
       '_id': '7',
-      'image': '/images/7.png',
+      'image': '/images/7.webp',
     },
     {
       '_id': '8', 
-      'image': '/images/8.png',
+      'image': '/images/8.webp',
     },
     {
       '_id': '9',
-      'image': '/images/9.png',
+      'image': '/images/9.webp',
     },
     {
       '_id': '10',
-      'image': '/images/10.png',
+      'image': '/images/10.webp',
     },
     {
       '_id': '11',
-      'image': '/images/11.png',
+      'image': '/images/11.webp',
     },
     {
       '_id': '12',
-      'image': '/images/12.png',
+      'image': '/images/12.webp',
     },
     {
       '_id': '13',
-      'image': '/images/13.png',
+      'image': '/images/13.webp',
     },
   ]
   
