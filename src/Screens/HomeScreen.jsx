e9ddefc9e693ae439d97dbd7898a8f23a1710203
NJP-10 <nicholasjeneperan@outlook.com>
2026-05-15 21:24:29 +0800

    changed background to webp. test

--- src/Screens/HomeScreen.jsx
+++ src/Screens/HomeScreen.jsx
@@ -6,13 +6,12 @@ import { ReactComponent as NameIcon } from './name.svg';
 function HomeScreen() {
   return (
     <div className='homescreen'>
-      <video autoPlay loop muted playsInline className='background-video'>
-        <source src="/bg.webm" type="video/mp4" />
-      </video>
+      <div className='bg-home'>
         <div className='svg-container'>
           <PortfolioIcon className="portfolio-image" />
           <NameIcon className="name-svg" />
         </div>
+      </div>
     </div>
   )
 }
